9baa85e956ed27f0fa8ef90af20d40920f118232
miskolc <calinho_gx1992@yahoo.com>
2012-11-25 01:58:42 +0200

    Updating users, authorization filters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update] 
+
   def show
   	@user=User.find(params[:id])
   end
@@ -21,4 +23,30 @@ class UsersController < ApplicationController
 
   end	
 
+  def edit
+  end  
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else  
+      render "edit"
+    end  
+  end  
+
+  private 
+    def signed_in_user
+      unless signed_in?  
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end  
+    end 
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end  
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do 
 	factory :user do
-		name  		"Michael Hartl"
-		email 		"mhartl@example.com"
+		name  		"Foobar Bazquux"
+		email 		"foobar@example.com"
 		password 	"foobar"
 		password_confirmation "foobar"
 	end	
